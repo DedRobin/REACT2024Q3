4cc192d42c44cb60f8c12072dc9b389aed901049
Pavel Rabekin <pavelrabekin@gmail.com>
2024-07-25 16:02:56 +0300

    feat: integrate RTK Query (implement 'apiSLice')

--- class-components/src/app/store.ts
+++ class-components/src/app/store.ts
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import resultReducer from "../components/Result/slice";
+import { starWarsApi } from "../store/apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     results: resultReducer,
+    [starWarsApi.reducerPath]: starWarsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(starWarsApi.middleware),
 });
+
+setupListeners(store.dispatch);
---
+++ class-components/src/store/apiSlice.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type SWPerson = { [key: string]: string };
+
+export const starWarsApi = createApi({
+  reducerPath: "starWarsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://swapi.dev/api/" }),
+  endpoints: (builder) => ({
+    getCharacters: builder.query<SWPerson, URLSearchParams>({
+      query: (searchParams) => {
+        if (searchParams) {
+          return `people/?${searchParams}`;
+        }
+        return `people/`;
+      },
+    }),
+    getCharacterById: builder.query<SWPerson, string>({
+      query: (id) => `people/${id}`,
+    }),
+  }),
+});
+
+export const { useGetCharacterByIdQuery, useGetCharactersQuery } = starWarsApi;
