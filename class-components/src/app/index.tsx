6fa279f0f1133ac6c6bd4a1d82a794b201c270e9
Pavel Rabekin <pavelrabekin@gmail.com>
2024-07-28 22:51:36 +0300

    feat: add theme button

--- class-components/src/app/index.tsx
+++ class-components/src/app/index.tsx
@@ -2,7 +2,25 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "../views/root";
 
 import "./style.css";
+import ThemeSwitch from "../components/ThemeSwitch";
+import { SyntheticEvent, useCallback, useState } from "react";
+import { Theme, ThemeContex } from "./contex";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const switchTheme = useCallback((event: SyntheticEvent) => {
+    const { currentTarget } = event;
+    if (currentTarget instanceof HTMLElement) {
+      if (currentTarget.className.includes("dark")) setTheme("light");
+      else setTheme("dark");
+    }
+  }, []);
+
+  return (
+    <ThemeContex.Provider value={theme}>
+      <ThemeSwitch onClick={switchTheme} />
+      <RouterProvider router={router} />;
+    </ThemeContex.Provider>
+  );
 }
