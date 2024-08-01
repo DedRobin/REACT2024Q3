6b95e3323982fdd4729fc67240e908fafc7e01d3
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-01 15:34:39 +0300

    fix: update test for <App />

--- class-components/src/app/__test__/app.test.tsx
+++ class-components/src/app/__test__/app.test.tsx
@@ -1,16 +1,32 @@
 import { describe, expect, test } from "vitest";
 import "@testing-library/jest-dom/vitest";
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import App from "..";
 import { renderWithStoreProvider } from "../../../tests/utils";
 
 describe("<App/>", async () => {
+  const testId = "app-testid";
+
   test("<App/> should be mounted", () => {
     renderWithStoreProvider(
-      <div data-testid="app-testid">
+      <div data-testid={testId}>
         <App />
       </div>,
     );
     expect(screen.getByTestId("app-testid")).toBeInTheDocument();
+  });
+
+  test("Switch theme", () => {
+    const { container } = renderWithStoreProvider(
+      <div data-testid={testId}>
+        <App />
+      </div>,
+    );
+    const button = container.querySelector(".theme-switch");
+    if (button) fireEvent.click(button);
+
+    const app = container.querySelector(".app");
+    expect(app).toBeInTheDocument();
+    expect((app as Element).className).toBe("app dark");
   });
 });
