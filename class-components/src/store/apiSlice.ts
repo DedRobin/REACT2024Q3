5167f7222a9169c15f7b8a21afc83eada11186a0
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-03 08:50:12 +0300

    fix: enable cache

--- class-components/src/store/apiSlice.ts
+++ class-components/src/store/apiSlice.ts
@@ -21,7 +21,6 @@ export type SwapiResponse = {
 export const starWarsApi = createApi({
   reducerPath: "starWarsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://swapi.dev/api/" }),
-  keepUnusedDataFor: 0,
   endpoints: (builder) => ({
     getCharacters: builder.query<SwapiResponse, string>({
       query: (searchParams) => {
