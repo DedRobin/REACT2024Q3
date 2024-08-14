5ac8c9c6752ff9942e91c65de8185f46c4505393
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-14 16:25:49 +0300

    feat: add autocomplete country input

--- src/components/Form/Country/index.tsx
+++ src/components/Form/Country/index.tsx
@@ -1,21 +1,52 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store";
+import { ChangeEventHandler, MouseEventHandler, useState } from "react";
 
 export default function CountryField() {
-  const countryList = useSelector((state: RootState) => state.country);
+  const [country, setCountry] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const countryList = useSelector((state: RootState) =>
+    state.country.filter((name) => searchValue && name.includes(searchValue)),
+  );
+
+  const onChange: ChangeEventHandler = ({ target }) => {
+    if (target instanceof HTMLInputElement) {
+      const { value } = target;
+      setSearchValue(value);
+      setCountry(value);
+    }
+  };
+
+  const onClick: MouseEventHandler = ({ target }) => {
+    if (
+      target instanceof HTMLLIElement &&
+      target.className === "country-item"
+    ) {
+      const { textContent } = target;
+      if (textContent) {
+        setCountry(textContent);
+        setSearchValue("");
+      }
+    }
+  };
 
   return (
     <div className="field country-field">
       <label className="country-label" htmlFor="country">
         Country
       </label>
-      <select id="country" className="country-select" name="country">
+      <input
+        id="country"
+        className="country-input"
+        name="country"
+        onChange={onChange}
+        value={country}
+      />
+      <ul className="autocomplete-country-list" onClick={onClick}>
         {countryList.map((countryName) => (
-          <option className="country-option">{countryName}</option>
+          <li className="country-item">{countryName}</li>
         ))}
-        <option className="country-option">Belarus</option>
-        <option className="country-option">Russia</option>
-      </select>
+      </ul>
     </div>
   );
 }
